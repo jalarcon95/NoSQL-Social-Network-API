8b7a6a664b85cbe708c4fbe77dcc1aa8f1473a9d
John Alarcon <j.alarcon@outlook.com>
2023-08-24 15:54:58 -0400

    troubleshooting

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -10,10 +10,17 @@ const {
 
 } = require('../../controllers/user-controller');
 
-router.route('/').get(getAllUser).post(createUser);
+router.route('/')
+.get(getAllUser)
+.post(createUser);
 
-router.route['/:id'].get(getUserById).put(updateUser).delete(deleteUser);
+router.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
 
-router.route('/userId/friends/:friendId').post(addFriend).delete(removeFriend);
+router.route('/userId/friends/:friendId')
+.post(addFriend)
+.delete(removeFriend);
 
 module.exports = router;
