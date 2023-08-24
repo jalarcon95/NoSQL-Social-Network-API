a2761fd877dfbbd9755fc5491ec0251e7316355f
John Alarcon <j.alarcon@outlook.com>
2023-08-24 15:15:22 -0400

    moved reaction object to thought file

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,38 @@
-const {Schema, model} = require('mongoose');
-const {Reaction} = require('../models/Reaction');
+const {Schema, model, Types} = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        responseBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+
+            get: (timestamp) => dateFormat(timestamp),
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+    },
+
+    {
+        toJSON: {
+            getters: true,
+          },
+          id: false,
+    }
+);
 
 const ThoughtSchema = new Schema(
     {
@@ -23,7 +55,7 @@ const ThoughtSchema = new Schema(
             required: true,
         },
 
-        reactions: [Reaction],
+        reactions: [ReactionSchema],
     },
     {
         toJSON: {
